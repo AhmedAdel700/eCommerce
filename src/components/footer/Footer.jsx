e6035d3f91d696be0393b49811f4df2561de879f
Ahmed Adel <AhmedAdel.Engineer1@gmail.com>
2024-10-06 22:33:03 +0200

    Finished The Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,18 +1,28 @@
 import Grid from "@mui/material/Grid2";
-
-import "./footer.css";
 import { Box, Stack } from "@mui/material";
+import CopyrightIcon from "@mui/icons-material/Copyright";
+import SendIcon from "@mui/icons-material/Send";
+import "./footer.css";
 
 export default function Footer() {
   return (
     <footer>
-      <Grid container spacing={10}>
+      <Grid container spacing={10} sx={{ marginBottom: "5rem" }}>
         <Grid item xs={12} sm={6} md={2} lg={2}>
           <Stack gap={3}>
             <h3>Exclusive</h3>
             <h4>Subscribe</h4>
             <p>Get 10% off your first order</p>
-            <input type="email" placeholder="Enter your email" />
+
+            <Box sx={{ position: "relative" }}>
+              <input
+                style={{ width: "100%" }}
+                type="email"
+                placeholder="Enter your email"
+                className="final-email"
+              />
+              <SendIcon className="send-arrow" />
+            </Box>
           </Stack>
         </Grid>
 
@@ -49,22 +59,50 @@ export default function Footer() {
           <Stack gap={3}>
             <h4>Download App</h4>
             <p>Save $3 with App New User Only</p>
-            <img
-              style={{ cursor: "pointer" }}
-              src="../images/Qr.png"
-              alt="Q Code"
-              draggable="false"
-            />
 
-            <Box>
-              <img src="" alt="" />
-              <img src="" alt="" />
-              <img src="" alt="" />
-              <img src="" alt="" />
-            </Box>
+            <Stack direction={"row"} gap={1}>
+              <img
+                style={{ cursor: "pointer" }}
+                src="../images/Qr.png"
+                alt="Q Code"
+                draggable="false"
+              />
+
+              <Stack gap={1}>
+                <img
+                  src="../images/store.png"
+                  alt="google store"
+                  draggable="false"
+                  style={{ cursor: "pointer" }}
+                />
+                <img
+                  src="../images/appstore.png"
+                  alt="app store"
+                  draggable="false"
+                  style={{ cursor: "pointer" }}
+                />
+              </Stack>
+            </Stack>
+
+            <Stack
+              direction={"row"}
+              gap={3}
+              alignItems={"center"}
+              className="icons-footer"
+            >
+              <img src="../images/Icon-Facebook.png" alt="" draggable="false" />
+              <img src="../images/Icon-t.png" alt="" draggable="false" />
+              <img src="../images/insta.png" alt="" draggable="false" />
+              <img src="../images/Vector.png" alt="" draggable="false" />
+            </Stack>
           </Stack>
         </Grid>
       </Grid>
+
+      <div className="copyright">
+        <CopyrightIcon />
+        Copyright Rimel 2022. All right reserved
+      </div>
     </footer>
   );
 }
