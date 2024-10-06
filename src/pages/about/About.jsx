397d8371786903e095da902644ffd364617653bd
Ahmed Adel <AhmedAdel.Engineer1@gmail.com>
2024-10-06 13:42:17 +0200

    Created The About & Contact Pages

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,4 +1,179 @@
-import './about.css';
+import { Box, Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+import "./about.css";
+
 export default function About() {
-  return <div>About Page</div>;
+  const arrayOfServices = [
+    {
+      img: "../../images/Services.png",
+      num: "10.5k",
+      type: "Sallers active our site",
+    },
+    {
+      img: "../../images/Services (1).png",
+      num: "33k",
+      type: "Mopnthly Produduct Sale",
+    },
+    {
+      img: "../../images/Services (2).png",
+      num: "45.5k",
+      type: "Customer active in our site",
+    },
+    {
+      img: "../../images/Services (3).png",
+      num: "25k",
+      type: "Anual gross sale in our site",
+    },
+  ];
+
+  const services = arrayOfServices.map((service) => {
+    return (
+      <div className="service" key={service.num}>
+        <img loading="lazy" src={service.img} alt={service.type} />
+        <h2>{service.num}</h2>
+        <p>{service.type}</p>
+      </div>
+    );
+  });
+
+  const teamData = [
+    {
+      img: "../../images/Frame 874.png",
+      name: "Tom Cruise",
+      position: "Founder & Chairman",
+      icon1: "../../images/Group.png",
+      icon2: "../../images/Group (1).png",
+      icon3: "../../images/Group (2).png",
+    },
+    {
+      img: "../../images/Frame 875.png",
+      name: "Emma Watson",
+      position: "Managing Director",
+      icon1: "../../images/Group.png",
+      icon2: "../../images/Group (1).png",
+      icon3: "../../images/Group (2).png",
+    },
+    {
+      img: "../../images/Frame 876.png",
+      name: "Will Smith",
+      position: "Product Designer",
+      icon1: "../../images/Group.png",
+      icon2: "../../images/Group (1).png",
+      icon3: "../../images/Group (2).png",
+    },
+  ];
+
+  const team = teamData.map((memeber) => {
+    return (
+      <div key={memeber.name} className="memeber">
+        <img src={memeber.img} alt={memeber.name} loading="lazy" />
+        <h2>{memeber.name}</h2>
+        <p>{memeber.position}</p>
+
+        <div className="social-media">
+          <img src={memeber.icon1} alt="X" />
+          <img src={memeber.icon2} alt="Instagram" />
+          <img src={memeber.icon3} alt="Linkedin" />
+        </div>
+      </div>
+    );
+  });
+
+  const arrayOfBenefits = [
+    {
+      img: "../../images/Services (4).png",
+      name: "FREE AND FAST DELIVERY",
+      text: "Free delivery for all orders over $140",
+    },
+    {
+      img: "../../images/Services (5).png",
+      name: "24/7 CUSTOMER SERVICE",
+      text: "Friendly 24/7 customer support",
+    },
+    {
+      img: "../../images/Services (6).png",
+      name: "MONEY BACK GUARANTEE",
+      text: "We reurn money within 30 days",
+    },
+  ];
+
+  const benefits = arrayOfBenefits.map((benefit) => {
+    return (
+      <div key={benefit.name} className="benefit">
+        <img src={benefit.img} alt={benefit.name} loading="lazy" />
+        <h2>{benefit.name}</h2>
+        <p>{benefit.text}</p>
+      </div>
+    );
+  });
+
+  return (
+    <section className="about">
+      <Stack direction={"row"} alignItems={"center"} className="link">
+        <Link
+          to="/"
+          style={{
+            color: "var(--text-color)",
+            marginRight: "10px",
+          }}
+        >
+          Home
+        </Link>
+        / About
+      </Stack>
+
+      <Box className="contanier">
+        <article>
+          <h1>Our Story</h1>
+
+          <p>
+            Launced in 2015, Exclusive is South Asia’s premier online shopping
+            makterplace with an active presense in Bangladesh. Supported by wide
+            range of tailored marketing, data and service solutions, Exclusive
+            has 10,500 sallers and 300 brands and serves 3 millioons customers
+            across the region
+          </p>
+
+          <p>
+            Exclusive has more than 1 Million products to offer, growing at a
+            very fast. Exclusive offers a diverse assotment in categories
+            ranging from consumer.
+          </p>
+        </article>
+
+        <aside>
+          <img loading="lazy" src="../../images/girls.png" alt="girls pic" />
+        </aside>
+      </Box>
+
+      <Box className="services"> {services}</Box>
+
+      <Stack
+        direction={"row"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+        className="team"
+      >
+        {team}
+      </Stack>
+
+      <Stack
+        className="dots"
+        direction={"row"}
+        justifyContent={"center"}
+        gap={2}
+        sx={{ margin: "0 auto" }}
+      >
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </Stack>
+
+      <Stack direction={"row"} justifyContent={"center"}className="our-benefits">
+        {benefits}
+      </Stack>
+    </section>
+  );
 }
--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,5 +1,153 @@
-import './contact.css';
+import { Box, Button, Stack } from "@mui/material";
+import Alert from "@mui/material/Alert";
+
+import { Link } from "react-router-dom";
+
+import { useState } from "react";
+
+import "./contact.css";
 
 export default function Contact() {
-  return <div>Contact Page</div>;
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    number: "",
+    message: "",
+  });
+
+  const [submit, setSubmit] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form Data:", data);
+
+    // Clear the form after submission
+    setData({ name: "", email: "", number: "", message: "" });
+
+    setSubmit(true);
+
+    setTimeout(() => {
+      setSubmit(false);
+    }, 2000);
+  };
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <section className="contact">
+      <Stack direction={"row"} alignItems={"center"} className="link">
+        <Link
+          to="/"
+          style={{
+            color: "var(--text-color)",
+            marginRight: "10px",
+          }}
+        >
+          Home
+        </Link>
+        / Contact
+      </Stack>
+
+      <Box className="sides">
+        <Stack direction={"column"} gap={4} className="left-side">
+          <Stack
+            direction={"row"}
+            alignItems={"center"}
+            gap={2}
+            className="img-text"
+          >
+            <img src="../../images/phone.png" alt="phone-icon" />
+            <h4>Call To Us</h4>
+          </Stack>
+
+          <p>We are available 24/7, 7 days a week.</p>
+          <p>Phone: +8801611112222</p>
+
+          <hr />
+
+          <Stack
+            direction={"row"}
+            alignItems={"center"}
+            gap={2}
+            className="img-text"
+          >
+            <img src="../../images/mail.png" alt="mail-icon" />
+            <h4>Write To US</h4>
+          </Stack>
+
+          <p>Fill out our form and we will contact you within 24 hours.</p>
+          <p>Emails: customer@exclusive.com</p>
+          <p>Emails: support@exclusive.com</p>
+        </Stack>
+
+        <Box className="right-side">
+          <form onSubmit={handleSubmit}>
+            <div className="inputs">
+              <input
+                type="text"
+                name="name"
+                value={data.name}
+                placeholder="Your Name"
+                required
+                onChange={handleChange}
+                autoComplete="off"
+              />
+              <input
+                type="email"
+                name="email"
+                value={data.email}
+                placeholder="Your Email"
+                required
+                onChange={handleChange}
+                autoComplete="off"
+              />
+              <input
+                type="number"
+                name="number"
+                value={data.number}
+                placeholder="Your Phone"
+                required
+                onChange={handleChange}
+                autoComplete="off"
+              />
+            </div>
+
+            <textarea
+              name="message"
+              value={data.message}
+              placeholder="Your Message"
+              required
+              onChange={handleChange}
+            ></textarea>
+
+            <Button
+              type="submit"
+              variant="contained"
+              sx={{ backgroundColor: "var(--red-color)", color: "white" }}
+            >
+              Send Message
+            </Button>
+
+            {submit && (
+              <Stack
+                className="alert"
+                sx={{ width: "100%", margin: "0 auto" }}
+                spacing={2}
+              >
+                <Alert
+                  sx={{ fontSize: "0.9rem", fontWeight: "600" }}
+                  variant="standard"
+                  severity="success"
+                >
+                  Message Sent Successfully !
+                </Alert>
+              </Stack>
+            )}
+          </form>
+        </Box>
+      </Box>
+    </section>
+  );
 }
