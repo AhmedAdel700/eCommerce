c31adf35d3f9b8200052a946d0f19c7f8275e909
Ahmed Adel <AhmedAdel.Engineer1@gmail.com>
2024-10-06 18:14:42 +0200

    Added Footer And Sign-up Pages

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,70 @@
-import './footer.css';
+import Grid from "@mui/material/Grid2";
+
+import "./footer.css";
+import { Box, Stack } from "@mui/material";
+
 export default function Footer() {
   return (
     <footer>
-        This Is A Footer
+      <Grid container spacing={10}>
+        <Grid item xs={12} sm={6} md={2} lg={2}>
+          <Stack gap={3}>
+            <h3>Exclusive</h3>
+            <h4>Subscribe</h4>
+            <p>Get 10% off your first order</p>
+            <input type="email" placeholder="Enter your email" />
+          </Stack>
+        </Grid>
+
+        <Grid item xs={12} sm={6} md={2} lg={2}>
+          <Stack gap={3}>
+            <h4>Support</h4>
+            <p>111 Bijoy sarani, Dhaka, DH 1515, Bangladesh.</p>
+            <p>exclusive@gmail.com</p>
+            <p>+88015-88888-9999</p>
+          </Stack>
+        </Grid>
+
+        <Grid item xs={12} sm={6} md={2} lg={2}>
+          <Stack gap={3}>
+            <h4>Account</h4>
+            <h5>My Account</h5>
+            <p>Login / Register</p>
+            <p>Cart</p>
+            <p>Wishlist</p>
+            <p>Shop</p>
+          </Stack>
+        </Grid>
+        <Grid item xs={12} sm={6} md={2} lg={2}>
+          <Stack gap={3}>
+            <h4>Quick Link</h4>
+            <p>Privacy Policy</p>
+            <p>Terms Of Use</p>
+            <p>FAQ</p>
+            <p>Contact</p>
+          </Stack>
+        </Grid>
+
+        <Grid item xs={12} direction={"column"} sm={6} md={2} lg={2}>
+          <Stack gap={3}>
+            <h4>Download App</h4>
+            <p>Save $3 with App New User Only</p>
+            <img
+              style={{ cursor: "pointer" }}
+              src="../images/Qr.png"
+              alt="Q Code"
+              draggable="false"
+            />
+
+            <Box>
+              <img src="" alt="" />
+              <img src="" alt="" />
+              <img src="" alt="" />
+              <img src="" alt="" />
+            </Box>
+          </Stack>
+        </Grid>
+      </Grid>
     </footer>
-  )
+  );
 }
--- src/pages/sign-up/SignUp.jsx
+++ src/pages/sign-up/SignUp.jsx
@@ -1,5 +1,99 @@
+import { Box, Button, Stack } from "@mui/material";
 import "./signUp.css";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function SginUp() {
-  return <div>Sign-Up Page</div>;
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form Data:", data);
+
+    // Clear the form after submission
+    setData({ name: "", email: "", password: "" });
+  };
+
+  const handleChnage = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <section className="sign-up">
+      <Box className="phone-img">
+        <img
+          src="../images/Side-Image.png"
+          alt="phone pic"
+        />
+      </Box>
+
+      <Stack className="create">
+        <Box className="box-create">
+          <h1>Create an account</h1>
+          <p>Enter your details below</p>
+
+          <form className="create-form" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="name"
+              value={data.name}
+              onChange={handleChnage}
+              placeholder="Name"
+              required
+            />
+            <input
+              type="email"
+              name="email"
+              value={data.email}
+              onChange={handleChnage}
+              placeholder="Email Or Phone Number"
+              required
+            />
+            <input
+              type="password"
+              name="password"
+              value={data.password}
+              onChange={handleChnage}
+              placeholder="Password"
+              required
+            />
+
+            <Button
+              type="submit"
+              variant="contained"
+              sx={{ backgroundColor: "var(--red-color)", color: "white" }}
+            >
+              Create Account
+            </Button>
+            <Button
+              variant="contained"
+              onClick={() => (window.location.href = "https://www.google.com/")}
+              sx={{
+                backgroundColor: "transparent",
+                color: "#000",
+                border: "1px solid #000",
+                marginBottom: "1rem",
+              }}
+            >
+              <Stack mr={1}>
+                <img src="../images/Icon-google.png" alt="google icon" />
+              </Stack>
+              Sign up with Google
+            </Button>
+          </form>
+
+          <Stack direction={"row"} gap={3} justifyContent={"center"}>
+            <p>Already have account?</p>
+            <Link to="/" style={{ textDecoration: "underline" }}>
+              Log in
+            </Link>
+          </Stack>
+        </Box>
+      </Stack>
+    </section>
+  );
 }
