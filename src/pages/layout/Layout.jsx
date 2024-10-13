15600848991e40d5c2a77de05d28cbcabe589b07
Ahmed Adel <AhmedAdel.Engineer1@gmail.com>
2024-10-13 22:43:59 +0200

    Fixed The Path Bug

--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import ScrollToTop from "../../components/ScrollToTop";
-import ScrollToTopButton from "../../components/scrollToTopButton";
+import ScrollToTopButton from "../../components/ScrollToTopButton";
 
 export default function Layout() {
   return (
